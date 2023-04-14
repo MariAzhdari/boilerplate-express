6dffb5282478bf899fcee4c6ac065c1c08806a96
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-14 12:37:03 +0100

    start with myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
+let port =3001;
+ 
 
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 
 
